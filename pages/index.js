e8ba7b96b973e7d1b44d6aec76c62b38df23aa38
AyushiJoshi27 <ayushi.joshi@newput.com>
2023-07-06 23:33:11 +0530

    optimization

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+  console.log(props)
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
+export default HomePage;
