67baf3d8fb8b04c5fd964bfd2620a1470f75d747
AyushiJoshi27 <ayushi.joshi@newput.com>
2023-07-25 16:26:41 +0530

    cookie

---
+++ components/Navbar.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import style from '../styles/Navbar.module.css'
+import Link from 'next/link'
+import AuthContext from '../context/AuthContext'
+
+export default function Navbar() {
+  const { logout, user } = useContext(AuthContext)
+
+  return (
+    <nav className={style.nav}>
+      <Link href="/"><h2><a>JWT-Auth-Nextjs</a></h2></Link>
+
+      <div>
+        <Link href="/login" legacyBehavior>
+          <a>Sign in</a>
+        </Link>
+        <Link href="/signup" legacyBehavior>
+          <a>Sign up</a>
+        </Link>
+        {user ? <>
+          <Link href="/dashboard" legacyBehavior>
+          <a>Dashboard</a>
+        </Link>
+        <div>
+          <a onClick={() => logout()}>Logout</a>
+        </div>
+        </> : null}
+      </div>
+        
+    </nav>
+  )
+}
---
+++ context/AuthContext.js
@@ -0,0 +1,72 @@
+import { createContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { NEXT_URL } from '@/config';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState('');
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [initialLoading, setInitialLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => checkUserLoggedIn(), []);
+
+  const login = async ({ email, password }) => {
+    setIsLoading(true);
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data);
+      router.push('/dashboard/');
+    } else {
+      setIsLoading(false);
+      setIsError(data.message);
+      setIsError(null);
+    }
+  };
+
+  const checkUserLoggedIn = async () => {
+    setInitialLoading(true);
+
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if(res.ok) {
+      setUser(data.user.data.user);
+    } else {
+      setUser(null);
+    }
+  };
+
+  const logout = async () => {
+    fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST',
+    })
+      .then((res) => {
+        setUser(null);
+        router.push('/login');
+      })
+      .catch((error) => {
+        console.error('error logging out user');
+      });
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isError, isLoading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
